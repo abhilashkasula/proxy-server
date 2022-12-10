e3c0f17d6a1ce6cdd09d8c2823420d1325d24b99
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-12-10 14:40:57 +0530

    Implement a simple proxy server

---
+++ server.js
@@ -0,0 +1,25 @@
+const {default: axios} = require('axios');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res) => {
+  axios({method: req.method, url: req.url, headers: req.header})
+    .catch(err => {
+      if (err.response) {
+        return err.response;
+      }
+      return {status: 500, data: ''};
+    })
+    .then(response => {
+      console.log(req.method, req.url, response.status);
+      Object.entries(response.headers).forEach(header =>
+        res.setHeader(header[0], header[1])
+      );
+      res.status(response.status).send(response.data);
+    });
+});
+
+app.listen(8000);
